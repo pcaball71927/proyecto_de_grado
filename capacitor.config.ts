7a66ca59d8c09e2f56c167e715c3aaf289bb0e5a
Paula Caballero <pcaball71927@universidadean.edu.co>
2022-03-03 22:09:26 -0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mercavis',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
